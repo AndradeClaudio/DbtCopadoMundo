0f5618e24939efa97723681f7804a0f9f00c6a40
Claudio de Andrade <andsil@gmail.com>
2022-12-05 17:29:49 -0300

    Incluindo previsões

---
+++ models/marts/fct_previsoes.sql
@@ -0,0 +1,23 @@
+WITH 
+    TIMES AS (
+        SELECT *
+            FROM {{ref('dim_times')}}
+        )
+    , PREVISOES AS 
+        (SELECT 
+                , chegar_na_oitava_de_finais 
+                , chegar_na_quarta_de_finais 
+                , chegar_na_semi_final 
+                , chegar_na_final 
+                , vencer_liga 
+            FROM {{ref('stg_base__previsoes.sql')}} 
+        )
+
+SELECT  DT.sk_time  AS sk_time
+    , chegar_na_oitava_de_finais 
+    , chegar_na_quarta_de_finais 
+    , chegar_na_semi_final 
+    , chegar_na_final 
+    , vencer_liga 
+    FROM PREVISOES STG
+    INNER JOIN TIMES DT ON DT.TIME=STG.TIME
