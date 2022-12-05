92c8c22698acf9ecc8f42d01bff5b86b025a2071
Claudio de Andrade <andsil@gmail.com>
2022-12-04 22:53:45 -0300

    Ajustes

--- models/staging/stg_base__previsoes.sql
+++ models/staging/stg_base__previsoes.sql
@@ -1,28 +1,28 @@
 with 
     source_wc_forecasts as 
     (
-    select  forecast_timestamp
-            , team
-            , group
-            , spi
-            , global_o
-            , global_d
-            , sim_wins
-            , sim_ties
-            , sim_losses
-            , sim_goal_diff
-            , goals_scored
-            , goals_against
-            , group_1
-            , group_2
-            , group_3
-            , group_4
-            , make_round_of_16
-            , make_quarters
-            , make_semis
-            , make_final
-            , win_league
-            , timestamp
+    select  cast (forecast_timestamp as date)
+            , cast (team as string)
+            , cast (group as tring
+            , cast (spi
+            , cast (global_o
+            , cast (global_d
+            , cast (sim_wins
+            , cast (sim_ties
+            , cast (sim_losses
+            , cast (sim_goal_diff
+            , cast (goals_scored
+            , cast (goals_against
+            , cast (group_1
+            , cast (group_2
+            , cast (group_3
+            , cast (group_4
+            , cast (make_round_of_16
+            , cast (make_quarters
+            , cast (make_semis
+            , cast (make_final
+            , cast (win_league
+            , cast (timestamp
     from {{source('base','wc_forecasts')}}
     )
 
